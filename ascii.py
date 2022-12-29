bc99db83efd1a3876584b836324f5e6fde1c47bc
l0rdByr0n <69580361+l0rdByr0n@users.noreply.github.com>
2022-12-29 14:44:02 +0100

    Create ascii.py

---
+++ ascii.py
@@ -0,0 +1,6 @@
+def convert_ascii(liste):
+    return "".join(chr(a) for a in liste)
+
+list = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
+
+print(convert_ascii(list))
